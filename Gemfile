924c74dfc40184dd21a204773329c95f7daf8afe
NealMullen <neal@ashfalldesign.com>
2013-12-26 14:29:04 +0000

    Before association

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sass-rails'
 gem 'will_paginate'
 gem 'jquery-rails'
 gem 'jquery-modal-rails'
+gem 'rails3-jquery-autocomplete'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 #gem 'turbolinks'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,21 +12,72 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require autocomplete-rails
 //= require_tree .
-
+$(function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#search" ).autocomplete({
+      source: availableTags,
+        select: function(event, ui) { 
+        $("#search").val(ui.item.label);
+        $("#posts_search").submit(); }
+    });
+  });
 
 if (history && history.pushState) {
 
   $(function() {
 
-$(document).on("click",".pagination a", function() {
+  $(document).on("click",".pagination a", function() {
 
-$.getScript(this.href);
-history.pushState(null, document.title, this.href);
- return false;
- 
+  $.getScript(this.href);
+  history.pushState(null, document.title, this.href);
+   return false;
+   
 
-});
+  });
+
+  $(document).on("click","a#all-posts", function() {
+
+  $.getScript(this.href);
+  history.pushState(null, document.title, this.href);
+   return false;
+   
+
+  });
+
+
+ // $(document).on("click","a.view-post", function() {
+
+ // $.getScript(this.href);
+ // history.pushState(null, document.title, this.href);
+ //  return false;
+   
+
+ // });
 
     $("#posts_search").submit(function() {
 
@@ -35,6 +86,9 @@ history.pushState(null, document.title, this.href);
        // scrollTop: $("#posts").offset().top
    // }, 640);
 $("#intro").slideUp(300);
+
+    $('input').blur();
+
       $.get($("#posts_search").attr("action"), $("#posts_search").serialize(), null, "script");
       history.pushState(null, document.title, $("#posts_search").attr("action") + "?" + $("#posts_search").serialize());
       
@@ -42,6 +96,8 @@ $("#intro").slideUp(300);
 
 
     });
+
+
     $(window).bind("popstate", function() {
       $.getScript(location.href);
     });
