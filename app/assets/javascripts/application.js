99e41021acbcbcbae29604b6e7f1b0116bff0c56
NealMullen <neal@ashfalldesign.com>
2013-12-27 01:07:45 +0000

    Nearly finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require jquery.modal
 //= require_tree .
-
+/*
 $(function() {
     var availableTags = [
       "ActionScript",
@@ -34,32 +34,63 @@ $(function() {
         $("#search").val(ui.item.label);
         $("#posts_search").submit(); }
     });
+
   });
+
+*/
+
+
+$(function() {
+   $.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _renderMenu: function( ul, items ) {
+      var that = this,
+        currentCategory = "";
+      $.each( items, function( index, item ) {
+        if ( item.category != currentCategory ) {
+          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+          currentCategory = item.category;
+        }
+        that._renderItemData( ul, item );
+      });
+    }
+  });
+   
+    var data = [
+      { label: "Electronics", category: "Categories" },
+      { label: "Books", category: "Categories" },
+      { label: "Misc", category: "Categories" },
+
+      { label: "Test search term", category: "Popular Search Terms" },
+      { label: "iPhone", category: "Popular Search Terms" },
+      { label: "headphones", category: "Popular Search Terms" },
+
+    ];
+ 
+    $( "#search" ).catcomplete({
+      delay: 0,
+      source: data,
+      select: function(event, ui) { 
+        $("#search").val(ui.item.label);
+        $("#posts_search").submit(); }
+    });
+  });
+
 
 if (history && history.pushState) {
 $(function() {
   $(document).on("click",".pagination a", function() {
     $.getScript(this.href);
     history.pushState(null, document.title, this.href);
+    $("html, body").animate({ scrollTop: 0 });
+    //window.scrollTo(0, 0);
     return false;
   });
-  $(document).on("click","a#all-posts", function() {
-    $.getScript(this.href);
-    history.pushState(null, document.title, this.href);
-    return false;
-  });
-  $(document).on("click","a.view-post", function() {
+  $(document).on("click","a.ajax-this-bitch", function() {
     $.getScript(this.href);
     history.pushState(null, document.title, this.href);
     return false;
   });
 
-
- $(document).on("click","a.back", function() {
-    $.getScript(this.href);
-    history.pushState(null, document.title, this.href);
-    return false;
-  });
 
   $("#posts_search").submit(function() {
     $("#intro").slideUp(300);
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ActiveRecord::Base
-	belongs_to  :category
+	belongs_to  :category 
 	belongs_to  :posttype
-	
 	#validates_presence_of :title, :description, :price, :category_id
 
 	def self.search(search)
 	  if search
-	    where('title LIKE ?', "%#{search}%")
+	   joins(:category).where('title LIKE ? OR categories.name LIKE ?', "%#{search}%","%#{search}%")
 	  else
 	    scoped
 	  end
