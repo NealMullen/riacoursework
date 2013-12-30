2130a3dacb6dfc9138ac74713fc9dc80e2c8170b
NealMullen <neal@ashfalldesign.com>
2013-12-30 22:21:38 +0000

    Precompiled for Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,6 +39,25 @@ $(function() {
 
 */
 
+ $(function() {
+
+  
+    $(document).tooltip({
+      items: '.tooltip '   ,
+      position: {
+        my: "center bottom-20",
+        at: "center top",
+        using: function( position, feedback ) {
+          $( this ).css( position );
+          $( "<div>" )
+            .addClass( "arrow" )
+            .addClass( feedback.vertical )
+            .addClass( feedback.horizontal )
+            .appendTo( this );
+        }
+      }
+    });
+  });
 
 $(function() {
    $.widget( "custom.catcomplete", $.ui.autocomplete, {
